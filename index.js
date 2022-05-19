f5a0e7ed8414a13b21ab8c9f163fc28215697b27
Ebere Okezie <ebereokezie@gmail.com>
2022-05-18 22:53:45 -0500

    commit tree 8caeb91fffabd65ebdf5c472e18cea4467f6a720

--- index.js
+++ index.js
@@ -1,4 +1,59 @@
 /* Your Code Here */
+function createEmployeeRecord(employee){
+    const employeeRecord = {}
+    Object.assign(employeeRecord, {
+        firstName: `${employee[0]}`,
+        familyName: `${employee[1]}`,
+        title: `${employee[2]}`,
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+     })
+    
+    return employeeRecord
+}
+
+function createEmployeeRecords(employees){
+    const employeeRecords = employees.map(employee => createEmployeeRecord(employee))
+    return employeeRecords
+}
+
+function createTimeInEvent(dateStamp){
+    const splitDateStamp = dateStamp.split(" ")
+    const timeIn = {}
+    Object.assign(timeIn, {
+        type: "TimeIn",
+        hour: parseInt(splitDateStamp[1]),
+        date: splitDateStamp[0]
+    })
+    this.timeInEvents.push(timeIn)
+    return this 
+}
+
+function createTimeOutEvent(dateStamp){
+    const splitDateStamp = dateStamp.split(" ")
+    const timeOut = {}
+    Object.assign(timeOut, {
+        type: "TimeOut",
+        hour: parseInt(splitDateStamp[1]),
+        date: splitDateStamp[0]
+    })
+    this.timeOutEvents.push(timeOut)
+    return this 
+}
+
+function hoursWorkedOnDate(date){
+    let startTime = this.timeInEvents.find(timeInEvent=> timeInEvent.date === date)
+    let endTime = this.timeOutEvents.find(timeOutEvent => timeOutEvent.date === date)
+
+    return (endTime.hour - startTime.hour)/100
+}
+
+function wagesEarnedOnDate(date){
+    let wages = this.payPerHour
+    return (hoursWorkedOnDate.call(this, date))*wages
+}
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -21,3 +76,11 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName(collection, firstNameString){
+    const foundEmployee = collection.find(emplRec => emplRec.firstName === firstNameString)
+    return foundEmployee
+}
+
+function calculatePayroll(employees){
+    return  employees.reduce((total, sum) => {return total+ allWagesFor.call(sum)}, 0)
+}
